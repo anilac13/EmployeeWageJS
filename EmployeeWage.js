354bf7c1273f24605e98de5bb12472112fcd74c1
anilac13 <anil4u2496@gmail.com>
2023-06-19 15:25:16 +0530

    [Anil] Add:Refactor code to get emp hours using function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,20 +1,24 @@
 const IS_PART_TIME = 2, IS_FULL_TIME = 1, PART_TIME_HRS = 4, FULL_TIME_HRS = 8, WAGE_PER_HR = 20;
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck)
+function getEmpHrs(empCheck)
 {
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        console.log("Employee present with Full time");
-        break;
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
-        console.log("Employee present with Part time");
-        break;
-    default:
-        empHrs = 0;
-        console.log("Employee is Absent");
-        break;
+    switch (empCheck)
+    {
+        case IS_FULL_TIME:
+            console.log("Employee present with Full time");
+            return FULL_TIME_HRS;
+            break;
+        case IS_PART_TIME:
+            console.log("Employee present with Part time");
+            return PART_TIME_HRS;
+            break;
+        default:
+            console.log("Employee is Absent");
+            return 0;
+            break;
+    }
 }
+empHrs = getEmpHrs(empCheck);
 let Daily_Wage = empHrs * WAGE_PER_HR;
-console.log("Daily Employee wage is: " + Daily_Wage)
+console.log("Daily Employee wage is: " + Daily_Wage);
