460153b868c1d39c43f01c77f140355dcf060f60
anilac13 <anil4u2496@gmail.com>
2023-06-19 22:13:00 +0530

    [Anil] Add:Using map, store daily wage and total wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,8 +2,12 @@ const IS_PART_TIME = 2, IS_FULL_TIME = 1, PART_TIME_HRS = 4, FULL_TIME_HRS = 8, 
 const NUM_OF_WORKINGDAYS = 20, TOTAL_WORKIG_HRS = 160;
 let totlEmpHrs = 0, totalWorkingDays = 0;
 let empHrs = 0;
-let EmpWageArray = new Array();
 let dayCount = 0;
+
+let EmpWageArray = new Array();
+let empDailyWageMap = new Map();
+let empDailyHrsMap = new Map();
+
 while (totlEmpHrs <= TOTAL_WORKIG_HRS && totalWorkingDays < NUM_OF_WORKINGDAYS)
 {
     totalWorkingDays++;
@@ -11,6 +15,9 @@ while (totlEmpHrs <= TOTAL_WORKIG_HRS && totalWorkingDays < NUM_OF_WORKINGDAYS)
     totlEmpHrs += getEmpHrs(empCheck);
     empHrs = getEmpHrs(empCheck);
     EmpWageArray.push(calculateWage(empHrs));
+
+    empDailyHrsMap.set(totalWorkingDays, empHrs);
+    empDailyWageMap.set(totalWorkingDays, calculateWage(empHrs));
 }
 function getEmpHrs(empCheck)
 {
@@ -49,10 +56,32 @@ function findNumOfDays(noOfDays, calculateWage)
     if (calculateWage > 0) return noOfDays+1;
     return noOfDays;
 }
+function totalWages(totalWage, dailyWage)
+{
+    return totalWage + dailyWage;
+}
+
+console.log("========================================");
+console.log("Employee daily wage array:");
+console.log("========================================");
+console.log(EmpWageArray);
+console.log("========================================");
+console.log("Employee daily Hrs Map:");
+console.log("========================================");
+console.log(empDailyHrsMap);
+console.log("========================================");
+console.log("Employee daily wage Map");
+console.log("========================================");
+console.log(empDailyWageMap);
+
+console.log("=========================================");
+console.log("Total employee wage using Map");
+console.log("------------------------------------------");
+console.log(Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
 
 let Emp_Wage = totlEmpHrs * WAGE_PER_HR;
-console.log("Total working days: "+ totalWorkingDays +"\nTotal working Hrs: "+ totlEmpHrs +"\nTotal employee wage is: " + Emp_Wage);
-console.log(EmpWageArray);
+console.log("\nTotal working days: "+ totalWorkingDays +"\nTotal working Hrs: "+ totlEmpHrs +"\nTotal employee wage is: " + Emp_Wage);
+//console.log(EmpWageArray);
 
 console.log("=============================================");
 const reducedArray = EmpWageArray.reduce((total, current) => (total + current));
