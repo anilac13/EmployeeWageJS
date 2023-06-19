872f33c7cf118ea8cdd1129eee348cac481f513a
anilac13 <anil4u2496@gmail.com>
2023-06-19 16:03:00 +0530

    [Anil] Add:Calculate wages till condition, 20 working days and 160 Hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,28 +1,26 @@
 const IS_PART_TIME = 2, IS_FULL_TIME = 1, PART_TIME_HRS = 4, FULL_TIME_HRS = 8, WAGE_PER_HR = 20;
-let empHrs = 0;
-const NUM_OF_WORKINGDAYS = 20;
-for (let day = 0; day < NUM_OF_WORKINGDAYS; day++)
+const NUM_OF_WORKINGDAYS = 20, TOTAL_WORKIG_HRS = 160;
+let totlEmpHrs = 0, totalWorkingDays = 0;
+while (totlEmpHrs <= TOTAL_WORKIG_HRS && totalWorkingDays < NUM_OF_WORKINGDAYS)
 {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getEmpHrs(empCheck);
+    totlEmpHrs += getEmpHrs(empCheck);
 }
 function getEmpHrs(empCheck)
 {
     switch (empCheck)
     {
         case IS_FULL_TIME:
-            console.log("Employee present with Full time");
             return FULL_TIME_HRS;
             break;
         case IS_PART_TIME:
-            console.log("Employee present with Part time");
             return PART_TIME_HRS;
             break;
         default:
-            console.log("Employee is Absent");
             return 0;
             break;
     }
 }
-let Emp_Wage = empHrs * WAGE_PER_HR;
-console.log("Total Employee wage is: " + Emp_Wage);
+let Emp_Wage = totlEmpHrs * WAGE_PER_HR;
+console.log("Total working days: "+ totalWorkingDays +"\nTotal working Hrs: "+ totlEmpHrs +"\nEmployee wage is: " + Emp_Wage);
