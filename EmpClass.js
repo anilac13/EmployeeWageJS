b368dd3595e0395a2bb06d270ba25b27f6ccca43
anilac13 <anil4u2496@gmail.com>
2023-06-21 13:10:24 +0530

    [Anil] Add:Check the name is valid or not using Regex pattern and use try, catch

--- EmpClass.js
+++ EmpClass.js
@@ -14,7 +14,10 @@ class Employee{
 
     get name() { return this._name; }
     set name(name){
+        let nameRegex = RegExp('^[A-Z][a-zA-Z]{2,}$');
+        if (nameRegex.test(name))
         this._name = name;
+        else throw "Name is Invalid";
     }
 
     toString(){
@@ -23,6 +26,7 @@ class Employee{
         return "Id = "+this.id+", Name = "+this.name+", Salary = "+this.salary+", Gender = "+this.gender+", StartDate = "+empDate;
     }
 }
+
 let employee = new Employee(1, "Amaron", 3000);
 console.log(employee.toString());
 employee.id = 2;
@@ -32,3 +36,15 @@ console.log(employee.toString());
 
 let employee1 = new Employee(3,"Ramya",2000,'F',new Date());
 console.log(employee1.toString());
+
+try{
+    employee1.id = 4
+    employee1.name = "Deamon";
+    employee1.salary = 4000;
+    employee1.gender = 'M';
+    employee1.startDate = new Date();
+    console.log(employee1.toString());
+}
+catch(error){
+    console.error(error);
+}
